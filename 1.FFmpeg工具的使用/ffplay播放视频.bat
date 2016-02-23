3a43dae4ce6baaec1454da001e86038a3c314a1f
yinwenjie <eternal.ywj@gmail.com>
2016-02-23 15:08:00 +0800

    ffplay.exe播放视频

---
+++ 1.FFmpeg工具的使用/ffplay播放视频.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+TITLE FFPlay
+
+IF NOT EXIST bin\ffplay.exe (
+  CLS
+  ECHO bin\ffplay.exe could not be found.
+  GOTO:error
+)
+
+ECHO ffplay found.
+CD bin || GOTO:error
+
+ffplay.exe -window_title "FFPlay Demo" -x 720 -y 576 -t 00:10 -autoexit -i ../video/IMG_0886.MOV
+
+:error
+ECHO.
+ECHO Press any key to exit.
+PAUSE >nul
+GOTO:EOF
