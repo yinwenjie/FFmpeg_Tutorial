592a0bfff110f80c4eeb945c2a6381081cf5c97d
yinwenjie <eternal.ywj@gmail.com>
2016-02-25 22:35:59 +0800

    添加源文件，修改工程设置

---
+++ 2.FFmpegSDK应用实例/VideoEncoding/VideoEncodingMain.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("*********************************\n");
+	printf("VideoEncoding: A FFmpeg SDK demo.\nDeveloped by Yin Wenjie\n");
+	printf("*********************************\n");
+	return 0;
+}
