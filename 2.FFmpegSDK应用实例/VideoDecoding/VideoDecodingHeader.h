758c823ff3c0ea65059050953e530f58441e0a0e
yinwenjie <eternal.ywj@gmail.com>
2016-03-05 21:32:03 +0800

    1、修改工程配置 2、添加头文件

--- 2.FFmpegSDK应用实例/VideoDecoding/VideoDecodingHeader.h
+++ 2.FFmpegSDK应用实例/VideoDecoding/VideoDecodingHeader.h
@@ -2,6 +2,15 @@
 #ifndef _VIDEO_DECODING_HEADER_
 #define _VIDEO_DECODING_HEADER_
 
-
+extern "C"
+{
+	#include "libavutil/opt.h"
+	#include "libavcodec/avcodec.h"
+	#include "libavutil/channel_layout.h"
+	#include "libavutil/common.h"
+	#include "libavutil/imgutils.h"
+	#include "libavutil/mathematics.h"
+	#include "libavutil/samplefmt.h"
+}
 
 #endif
