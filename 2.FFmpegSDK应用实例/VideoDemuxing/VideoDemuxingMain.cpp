23c9d9154ae7ad228d93f15f498255d9b3547f20
yinwenjie <eternal.ywj@gmail.com>
2016-03-14 14:39:31 +0800

    建立avformat对象、打开文件并初步分析

--- 2.FFmpegSDK应用实例/VideoDemuxing/VideoDemuxingMain.cpp
+++ 2.FFmpegSDK应用实例/VideoDemuxing/VideoDemuxingMain.cpp
@@ -1,6 +1,245 @@
 #include <stdio.h>
 
-int main()
+extern "C"
 {
+	#include "libavutil/opt.h"
+	#include "libavutil/channel_layout.h"
+	#include "libavutil/common.h"
+	#include "libavutil/imgutils.h"
+	#include "libavutil/mathematics.h"
+	#include "libavutil/samplefmt.h"
+	#include "libavformat/avformat.h"
+	#include "libavcodec/avcodec.h"
+}
+
+#define STREAM_DURATION   10.0
+#define STREAM_FRAME_RATE 25 /* 25 images/s */
+#define STREAM_PIX_FMT    AV_PIX_FMT_YUV420P /* default pix_fmt */
+
+// a wrapper around a single output AVStream
+typedef struct OutputStream 
+{
+	AVStream *st;
+
+	/* pts of the next frame that will be generated */
+	int64_t next_pts;
+	int samples_count;
+
+	AVFrame *frame;
+	AVFrame *tmp_frame;
+
+	float t, tincr, tincr2;
+
+	struct SwsContext *sws_ctx;
+	struct SwrContext *swr_ctx;
+} OutputStream;
+
+/* Add an output stream. */
+static void add_stream(OutputStream *ost, AVFormatContext *oc,
+                       AVCodec **codec,
+                       enum AVCodecID codec_id)
+{
+    AVCodecContext *c;
+    int i;
+	AVRational r1 = { 1, c->sample_rate }, r2 = { 1, STREAM_FRAME_RATE };
+
+    /* find the encoder */
+    *codec = avcodec_find_encoder(codec_id);
+    if (!(*codec)) {
+        fprintf(stderr, "Could not find encoder for '%s'\n",
+                avcodec_get_name(codec_id));
+        exit(1);
+    }
+
+    ost->st = avformat_new_stream(oc, *codec);
+    if (!ost->st) {
+        fprintf(stderr, "Could not allocate stream\n");
+        exit(1);
+    }
+    ost->st->id = oc->nb_streams-1;
+    c = ost->st->codec;
+
+    switch ((*codec)->type) {
+    case AVMEDIA_TYPE_AUDIO:
+        c->sample_fmt  = (*codec)->sample_fmts ?
+            (*codec)->sample_fmts[0] : AV_SAMPLE_FMT_FLTP;
+        c->bit_rate    = 64000;
+        c->sample_rate = 44100;
+        if ((*codec)->supported_samplerates) {
+            c->sample_rate = (*codec)->supported_samplerates[0];
+            for (i = 0; (*codec)->supported_samplerates[i]; i++) {
+                if ((*codec)->supported_samplerates[i] == 44100)
+                    c->sample_rate = 44100;
+            }
+        }
+        c->channels        = av_get_channel_layout_nb_channels(c->channel_layout);
+        c->channel_layout = AV_CH_LAYOUT_STEREO;
+        if ((*codec)->channel_layouts) {
+            c->channel_layout = (*codec)->channel_layouts[0];
+            for (i = 0; (*codec)->channel_layouts[i]; i++) {
+                if ((*codec)->channel_layouts[i] == AV_CH_LAYOUT_STEREO)
+                    c->channel_layout = AV_CH_LAYOUT_STEREO;
+            }
+        }
+        c->channels        = av_get_channel_layout_nb_channels(c->channel_layout);
+        ost->st->time_base = r1;
+        break;
+
+    case AVMEDIA_TYPE_VIDEO:
+        c->codec_id = codec_id;
+
+        c->bit_rate = 400000;
+        /* Resolution must be a multiple of two. */
+        c->width    = 352;
+        c->height   = 288;
+        /* timebase: This is the fundamental unit of time (in seconds) in terms
+         * of which frame timestamps are represented. For fixed-fps content,
+         * timebase should be 1/framerate and timestamp increments should be
+         * identical to 1. */
+        ost->st->time_base = r2;
+        c->time_base       = ost->st->time_base;
+
+        c->gop_size      = 12; /* emit one intra frame every twelve frames at most */
+        c->pix_fmt       = STREAM_PIX_FMT;
+        if (c->codec_id == AV_CODEC_ID_MPEG2VIDEO) {
+            /* just for testing, we also add B frames */
+            c->max_b_frames = 2;
+        }
+        if (c->codec_id == AV_CODEC_ID_MPEG1VIDEO) {
+            /* Needed to avoid using macroblocks in which some coeffs overflow.
+             * This does not happen with normal video, it just happens here as
+             * the motion of the chroma plane does not match the luma plane. */
+            c->mb_decision = 2;
+        }
+    break;
+
+    default:
+        break;
+    }
+
+    /* Some formats want stream headers to be separate. */
+    if (oc->oformat->flags & AVFMT_GLOBALHEADER)
+        c->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+}
+
+static AVFrame *alloc_picture(enum AVPixelFormat pix_fmt, int width, int height)
+{
+	AVFrame *picture;
+	int ret;
+
+	picture = av_frame_alloc();
+	if (!picture)
+		return NULL;
+
+	picture->format = pix_fmt;
+	picture->width  = width;
+	picture->height = height;
+
+	/* allocate the buffers for the frame data */
+	ret = av_frame_get_buffer(picture, 32);
+	if (ret < 0) {
+		fprintf(stderr, "Could not allocate frame data.\n");
+		exit(1);
+	}
+
+	return picture;
+}
+
+static void open_video(AVFormatContext *oc, AVCodec *codec, OutputStream *ost, AVDictionary *opt_arg)
+{
+    int ret;
+    AVCodecContext *c = ost->st->codec;
+    AVDictionary *opt = NULL;
+
+    av_dict_copy(&opt, opt_arg, 0);
+
+    /* open the codec */
+    ret = avcodec_open2(c, codec, &opt);
+    av_dict_free(&opt);
+    if (ret < 0) {
+        printf("Could not open video codec: %s\n", ret);
+        exit(1);
+    }
+
+    /* allocate and init a re-usable frame */
+    ost->frame = alloc_picture(c->pix_fmt, c->width, c->height);
+    if (!ost->frame) {
+        fprintf(stderr, "Could not allocate video frame\n");
+        exit(1);
+    }
+
+    /* If the output format is not YUV420P, then a temporary YUV420P
+     * picture is needed too. It is then converted to the required
+     * output format. */
+    ost->tmp_frame = NULL;
+    if (c->pix_fmt != AV_PIX_FMT_YUV420P) {
+        ost->tmp_frame = alloc_picture(AV_PIX_FMT_YUV420P, c->width, c->height);
+        if (!ost->tmp_frame) {
+            fprintf(stderr, "Could not allocate temporary picture\n");
+            exit(1);
+        }
+    }
+}
+
+
+
+int main(int argc, char **argv)
+{
+	OutputStream video_st = { 0 }, audio_st = { 0 };
+	const char *filename;
+	AVOutputFormat *fmt;
+	AVFormatContext *oc;
+	AVCodec *audio_codec, *video_codec;
+	int ret;
+	int have_video = 0, have_audio = 0;
+	int encode_video = 0, encode_audio = 0;
+	AVDictionary *opt = NULL;
+
+	/* Initialize libavcodec, and register all codecs and formats. */
+	av_register_all();
+
+	if (argc < 2) {
+		printf("usage: %s output_file\n"
+			"API example program to output a media file with libavformat.\n"
+			"This program generates a synthetic audio and video stream, encodes and\n"
+			"muxes them into a file named output_file.\n"
+			"The output format is automatically guessed according to the file extension.\n"
+			"Raw images can also be output by using '%%d' in the filename.\n"
+			"\n", argv[0]);
+		return 1;
+	}
+
+	filename = argv[1];
+	if (argc > 3 && !strcmp(argv[2], "-flags")) 
+	{
+		av_dict_set(&opt, argv[2]+1, argv[3], 0);
+	}
+
+	/* allocate the output media context */
+	avformat_alloc_output_context2(&oc, NULL, NULL, filename);
+	if (!oc) 
+	{
+		printf("Could not deduce output format from file extension: using MPEG.\n");
+		avformat_alloc_output_context2(&oc, NULL, "mpeg", filename);
+	}
+	if (!oc)
+		return 1;
+
+	fmt = oc->oformat;
+	/* Add the audio and video streams using the default format codecs
+     * and initialize the codecs. */
+    if (fmt->video_codec != AV_CODEC_ID_NONE) 
+	{
+        add_stream(&video_st, oc, &video_codec, fmt->video_codec);
+        have_video = 1;
+        encode_video = 1;
+    }
+    if (fmt->audio_codec != AV_CODEC_ID_NONE)
+	{
+        add_stream(&audio_st, oc, &audio_codec, fmt->audio_codec);
+        have_audio = 1;
+        encode_audio = 1;
+    }
+
 	return 0;
 }
