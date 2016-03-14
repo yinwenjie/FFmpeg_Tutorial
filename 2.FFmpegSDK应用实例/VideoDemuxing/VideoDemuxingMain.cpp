63be65915b1ad1be86cf6286d88fc66605313708
yinwenjie <eternal.ywj@gmail.com>
2016-03-14 12:42:09 +0800

    重置VideoDemuxing工程

--- 2.FFmpegSDK应用实例/VideoDemuxing/VideoDemuxingMain.cpp
+++ 2.FFmpegSDK应用实例/VideoDemuxing/VideoDemuxingMain.cpp
@@ -1,20 +1,6 @@
 #include <stdio.h>
-#include "VideoDemuxingHeader.h"
 
-int main(int argc, char **argv)
+int main()
 {
-	OutputStream video_st = { 0 }, audio_st = { 0 };
-	const char *filename;
-	AVOutputFormat *fmt;
-	AVFormatContext *oc;
-	AVCodec *audio_codec, *video_codec;
-	int ret;
-	int have_video = 0, have_audio = 0;
-	int encode_video = 0, encode_audio = 0;
-	AVDictionary *opt = NULL;
-
-	/* Initialize libavcodec, and register all codecs and formats. */
-//	av_register_all();
-
-	return 1;
+	return 0;
 }
