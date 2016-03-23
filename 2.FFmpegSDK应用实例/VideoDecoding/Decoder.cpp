e49d1e9253fac8690f5514a804c9bc5241cfc71b
yinwenjie <eternal.ywj@gmail.com>
2016-03-23 21:18:44 +0800

    其他一些注释

--- 2.FFmpegSDK应用实例/VideoDecoding/Decoder.cpp
+++ 2.FFmpegSDK应用实例/VideoDecoding/Decoder.cpp
@@ -3,9 +3,13 @@
 
 bool Open_deocder(CodecCtx &ctx)
 {
+	//注册编解码器对象
 	avcodec_register_all();	
+
+	//初始化AVPacket对象
 	av_init_packet(&(ctx.pkt));
 
+	//根据CODEC_ID查找AVCodec对象
 	ctx.pCodec = avcodec_find_decoder(AV_CODEC_ID_H264);
 	if (!ctx.pCodec) 
 	{
@@ -13,6 +17,7 @@ bool Open_deocder(CodecCtx &ctx)
 		return false;
 	}
 
+	//根据AVCodec对象分配AVCodecContext
 	ctx.pCodecContext = avcodec_alloc_context3(ctx.pCodec);
 	if (!ctx.pCodecContext)
 	{
@@ -23,6 +28,7 @@ bool Open_deocder(CodecCtx &ctx)
 	if (ctx.pCodec->capabilities & AV_CODEC_CAP_TRUNCATED)
 		ctx.pCodecContext->flags |= AV_CODEC_FLAG_TRUNCATED; // we do not send complete frames
 
+	//根据CODEC_ID初始化AVCodecParserContext对象
 	ctx.pCodecParserCtx = av_parser_init(AV_CODEC_ID_H264);
 	if (!ctx.pCodecParserCtx)
 	{
@@ -30,13 +36,14 @@ bool Open_deocder(CodecCtx &ctx)
 		return false;
 	}
 
-	/* open it */
+	//打开AVCodec对象
 	if (avcodec_open2(ctx.pCodecContext, ctx.pCodec, NULL) < 0)
 	{
 		fprintf(stderr, "Could not open codec\n");
 		return false;
 	}
 
+	//分配AVFrame对象
 	ctx.frame = av_frame_alloc();
 	if (!ctx.frame) 
 	{
