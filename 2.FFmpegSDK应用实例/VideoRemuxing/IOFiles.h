03b6877ec4fbc5805ddae54aa11cd1a9fde3c403
yinwenjie <eternal.ywj@gmail.com>
2016-05-19 23:24:49 +0800

    实现VideoRemuxing

---
+++ 2.FFmpegSDK应用实例/VideoRemuxing/IOFiles.h
@@ -0,0 +1,10 @@
+#ifndef _IO_FILES_H_
+#define _IO_FILES_H_
+
+typedef struct _IOFiles
+{
+	const char *inputName;
+	const char *outputName;
+} IOFiles;
+
+#endif
--- 2.FFmpegSDK应用实例/VideoRemuxing/VideoRemuxingMain.cpp
+++ 2.FFmpegSDK应用实例/VideoRemuxing/VideoRemuxingMain.cpp
@@ -66,6 +66,84 @@ int main(int argc, char **argv)
 	}
 	ofmt = ofmt_ctx->oformat;
 
+	for (unsigned int i = 0; i < ifmt_ctx->nb_streams ; i++)
+	{
+		AVStream *inStream = ifmt_ctx->streams[i];
+		AVStream *outStream = avformat_new_stream(ofmt_ctx, inStream->codec->codec);
+		if (!outStream)
+		{
+			printf("Error: Could not allocate output stream.\n");
+			goto end;
+		}
+
+		ret = avcodec_copy_context(outStream->codec, inStream->codec);
+		outStream->codec->codec_tag = 0;
+		if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+		{
+			outStream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+		}
+	}
+
+	av_dump_format(ofmt_ctx, 0, io_param.outputName, 1);
+
+	if (!(ofmt->flags & AVFMT_NOFILE))
+	{
+		ret = avio_open(&ofmt_ctx->pb, io_param.outputName, AVIO_FLAG_WRITE);
+		if (ret < 0)
+		{
+			printf("Error: Could not open output file.\n");
+			goto end;
+		}
+	}
+
+	ret = avformat_write_header(ofmt_ctx, NULL);
+	if (ret < 0) 
+	{
+		printf("Error: Could not write output file header.\n");
+		goto end;
+	}
+
+	while (1) 
+	{
+		AVStream *in_stream, *out_stream;
+
+		ret = av_read_frame(ifmt_ctx, &pkt);
+		if (ret < 0)
+			break;
+
+		in_stream  = ifmt_ctx->streams[pkt.stream_index];
+		out_stream = ofmt_ctx->streams[pkt.stream_index];
+		
+		/* copy packet */
+		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+		pkt.pos = -1;
+
+		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+		if (ret < 0) 
+		{
+			fprintf(stderr, "Error muxing packet\n");
+			break;
+		}
+		av_free_packet(&pkt);
+	}
+
+	av_write_trailer(ofmt_ctx);
+
 end:
+	avformat_close_input(&ifmt_ctx);
+
+	/* close output */
+	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+		avio_closep(&ofmt_ctx->pb);
+
+	avformat_free_context(ofmt_ctx);
+
+	if (ret < 0 && ret != AVERROR_EOF) 
+	{
+		fprintf(stderr, "Error failed to write packet to output file.\n");
+		return 1;
+	}
 	return 0;
 }
