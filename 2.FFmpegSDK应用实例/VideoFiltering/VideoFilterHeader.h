2a69237fbd37be09fd324e4df2b1847ef30d3802
yinwenjie <eternal.ywj@gmail.com>
2016-05-25 13:37:44 +0800

    解析命令行参数

---
+++ 2.FFmpegSDK应用实例/VideoFiltering/VideoFilterHeader.h
@@ -0,0 +1,17 @@
+#ifndef _VIDEO_FILTER_HEADER_H_
+#define _VIDEO_FILTER_HEADER_H_
+
+#define snprintf(buf,len, format,...) _snprintf_s(buf, len,len, format, __VA_ARGS__)
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libavfilter/avfiltergraph.h"
+#include "libavfilter/buffersink.h"
+#include "libavfilter/buffersrc.h"
+#include "libavutil/avutil.h"
+#include "libswscale/swscale.h"
+};
+
+#endif
