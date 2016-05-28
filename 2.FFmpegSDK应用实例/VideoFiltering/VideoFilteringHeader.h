0fad86197af3785ea087fa446af20f07d6ccea1c
yinwenjie <eternal.ywj@gmail.com>
2016-05-28 14:17:42 +0800

    添加VideoFiltering工程

---
+++ 2.FFmpegSDK应用实例/VideoFiltering/VideoFilteringHeader.h
@@ -0,0 +1,15 @@
+#ifndef _VIDEO_FILTERING_HEADER_H_
+#define _VIDEO_FILTERING_HEADER_H_
+
+#define snprintf(buf,len, format,...) _snprintf_s(buf, len,len, format, __VA_ARGS__)
+
+extern "C"
+{
+#include "libavfilter/avfiltergraph.h"  
+#include "libavfilter/buffersink.h"  
+#include "libavfilter/buffersrc.h"  
+#include "libavutil/avutil.h"  
+#include "libavutil/imgutils.h" 
+};
+
+#endif
