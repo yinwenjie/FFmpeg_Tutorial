78ef461c0c3afa218d1f4e959cd9f6668479719e
yinwenjie <eternal.ywj@gmail.com>
2016-05-30 11:06:02 +0800

    添加VideoScaling工程

---
+++ 2.FFmpegSDK应用实例/VideoScaling/VideoScalingHeader.h
@@ -0,0 +1,11 @@
+#ifndef _VIDEO_SCALING_HEADER_H_
+#define _VIDEO_SCALING_HEADER_H_
+
+extern "C"
+{
+#include "libavutil/imgutils.h"
+#include "libavutil/parseutils.h"
+#include "libswscale/swscale.h"
+};
+
+#endif
